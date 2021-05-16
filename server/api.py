f39dd0ba698379d5ca331df80269b38b77e35e2a
Davide Ghiotto <ghiotto.davidenko@gmail.com>
2021-05-16 12:13:52 +0200

    home + api test

--- server/api.py
+++ server/api.py
@@ -1,13 +1,14 @@
 import flask
+from flask import request
 from flask_cors import CORS, cross_origin
 
 
-# if __name__ == "__main__":
-#     from scraper import scrape
-#     from database import savePortfolio, getLastPortfolio, getAllPortfolios
-# else:
-#     from app.scraper import scrape
-#     from app.database import savePortfolio, getLastPortfolio, getAllPortfolios
+if __name__ == "__main__":
+    from predict import predict_ultra_chrome
+    from results import get_results
+else:
+    from server.predict import predict_ultra_chrome
+    from server.results import get_results
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -22,24 +23,19 @@ def home():
     return "<h1>ultra-chrome-server</h1><p>Backend server for ultra chrome demo webapp.</p>"
 
 
-# @app.route("/portfolio", methods=["GET"])
-# @cross_origin()
-# def portfolio():
-#     return getLastPortfolio()
+@app.route("/predict/ultra-chrome", methods=["POST"])
+@cross_origin()
+def predictUltraChrome():
+    cell_type = request.json.get("cell")
+    return predict_ultra_chrome(cell_type)
 
 
-# @app.route("/portfolio/all", methods=["GET"])
-# @cross_origin()
-# def allPortfolio():
-#     return getAllPortfolios()
-
-
-# @app.route("/portfolio/update", methods=["GET"])
-# @cross_origin()
-# def updatePortfolio():
-#     new_portfolio = scrape()
-#     print("new portfolio", new_portfolio)
-#     return savePortfolio(new_portfolio)
+@app.route("/results", methods=["GET"])
+@cross_origin()
+def getResults():
+    model = request.json.get("model")
+    is_separated = request.json.get("sep")
+    return get_results(model, is_separated)
 
 
 if __name__ == "__main__":
---
+++ server/predict.py
@@ -0,0 +1,3 @@
+def predict_ultra_chrome(cell_type):
+    print(cell_type)
+    return [1, 2, 3]
